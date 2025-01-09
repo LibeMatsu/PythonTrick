1a372daa12476d40b1ebb8f66c0473c12ee5b121
kayoko-matuski <matsuki.1067@universal-osaka.com>
2025-01-09 13:21:14 +0900

    *argsと**kwargs

--- 3.4_args_kwargs.py
+++ 3.4_args_kwargs.py
@@ -37,3 +37,22 @@ class AlwaysBlueCar(Car):
 
 
 print(AlwaysBlueCar('green', 48392).color)
+print('')
+
+
+# メタデータを引き継ぎつつwrap
+import functools
+
+def trace(f):
+    @functools.wraps(f)
+    def decorated_function(*args, **kwargs):
+        print(f, args, kwargs)
+        result = f(*args, **kwargs)
+        print(result)
+    return decorated_function
+
+@trace
+def greet(greeting, name):
+    return '{}, {}!'.format(greeting, name)
+
+greet('Hello', 'Bob')
